30af198a122d2f8c4c87f0c08b1b69054cb98b29
pedjo95 <tec.ped95@gmail.com>
2020-06-03 00:17:46 -0300

    Creating routes and using params(query, body..)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,37 @@
-import express from "express";
+import express, { request } from "express";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  console.log("Usuarios");
+app.use(express.json());
 
-  response.json(["Diego", "Pedro", "Caio", "Ayrton"]);
+const users = ["Diego", "Pedro", "Caio", "Ayrton"];
+
+app.get("/users", (request, response) => {
+  const search = String(request.query.search);
+
+  const filteredUsers = search
+    ? users.filter((user) => user.includes(search))
+    : users;
+
+  response.json(filteredUsers);
 });
 
+app.get("/users/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post("/users", (request, response) => {
+  const data = request.body;
+
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
+
+  return response.json(user);
+});
 app.listen(3333);
