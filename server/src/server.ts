51eded9dd69d0b7f5e9f6c5509079ec32655b5c9
pedjo95 <tec.ped95@gmail.com>
2020-06-03 03:34:05 -0300

    Adding CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import express, { request } from "express";
+import cors from "cors";
 import routes from "./routes";
 import path from "path";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
