7a04f5b7dd4fa6e83a1a94627557f86b3d033f9a
pedjo95 <tec.ped95@gmail.com>
2020-06-03 00:37:48 -0300

    Creating connection to database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,37 +1,10 @@
 import express, { request } from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-const users = ["Diego", "Pedro", "Caio", "Ayrton"];
+app.use(routes);
 
-app.get("/users", (request, response) => {
-  const search = String(request.query.search);
-
-  const filteredUsers = search
-    ? users.filter((user) => user.includes(search))
-    : users;
-
-  response.json(filteredUsers);
-});
-
-app.get("/users/:id", (request, response) => {
-  const id = Number(request.params.id);
-
-  const user = users[id];
-
-  return response.json(user);
-});
-
-app.post("/users", (request, response) => {
-  const data = request.body;
-
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-
-  return response.json(user);
-});
 app.listen(3333);
